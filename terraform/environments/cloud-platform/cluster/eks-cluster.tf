954c75528431c03abbd4da1e9865bfa1cd435d2d
tom-j-smith <thomas.smith@digital.justice.gov.uk>
2026-03-16 11:51:14 +0000

    Adding depends on to fix fresh cluster build

--- terraform/environments/cloud-platform/cluster/eks-cluster.tf
+++ terraform/environments/cloud-platform/cluster/eks-cluster.tf
@@ -196,5 +196,5 @@ data "kubectl_path_documents" "manifests" {
 resource "kubectl_manifest" "deploy_manifest" {
   # for_each  = data.kubectl_path_documents.manifests.manifests
   # yaml_body = each.value
-  yaml_body = data.kubectl_path_documents.manifests.manifests
+  yaml_body = data.kubectl_path_documents.manifests[0].manifests
 }
