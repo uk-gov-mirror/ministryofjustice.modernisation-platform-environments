3d134ae82f4dafcb5378f4faf9e176197eb4fd5e
Luke Harper <luke.harper4@justice.gov.uk>
2026-03-20 09:42:51 +0000

    CDIA-36 Destroy policy first.

--- terraform/environments/hmpps-court-data-ingestion-api/secrets.tf
+++ terraform/environments/hmpps-court-data-ingestion-api/secrets.tf
@@ -28,48 +28,48 @@ module "secret_ingestion_api_auth_token" {
   tags = local.tags
 }
 
-data "aws_iam_policy_document" "secret_ingestion_api_auth_token_policy_data" {
-  statement {
-    sid    = "AllowCrossAccountAccess"
-    effect = "Allow"
+# data "aws_iam_policy_document" "secret_ingestion_api_auth_token_policy_data" {
+#   statement {
+#     sid    = "AllowCrossAccountAccess"
+#     effect = "Allow"
 
-    principals {
-      type = "AWS"
-      identifiers = [
-        "arn:aws:iam::754256621582:role/cloud-platform-irsa-6852dfe05c1167f2-live"
-      ]
-    }
+#     principals {
+#       type = "AWS"
+#       identifiers = [
+#         "arn:aws:iam::754256621582:role/cloud-platform-irsa-6852dfe05c1167f2-live"
+#       ]
+#     }
 
-    actions = [
-      "secretsmanager:GetSecretValue",
-      "secretsmanager:PutSecretValue"
-    ]
+#     actions = [
+#       "secretsmanager:GetSecretValue",
+#       "secretsmanager:PutSecretValue"
+#     ]
 
-    resources = ["*"]
-  }
-  statement {
-    sid    = "AllowSelfAdmin"
-    effect = "Allow"
+#     resources = ["*"]
+#   }
+#   statement {
+#     sid    = "AllowSelfAdmin"
+#     effect = "Allow"
 
-    principals {
-      type = "AWS"
-      identifiers = [
-        "arn:aws:iam::953751538119:root"
-      ]
-    }
+#     principals {
+#       type = "AWS"
+#       identifiers = [
+#         "arn:aws:iam::953751538119:root"
+#       ]
+#     }
 
-    actions = [
-      "*"
-    ]
+#     actions = [
+#       "*"
+#     ]
 
-    resources = ["*"]
-  }
-}
+#     resources = ["*"]
+#   }
+# }
 
-resource "aws_secretsmanager_secret_policy" "secret_ingestion_api_auth_token_policy" {
-  secret_arn = module.secret_ingestion_api_auth_token.secret_arn
-  policy     = data.aws_iam_policy_document.secret_ingestion_api_auth_token_policy_data.json
-}
+# resource "aws_secretsmanager_secret_policy" "secret_ingestion_api_auth_token_policy" {
+#   secret_arn = module.secret_ingestion_api_auth_token.secret_arn
+#   policy     = data.aws_iam_policy_document.secret_ingestion_api_auth_token_policy_data.json
+# }
 
 
 #software.amazon.awssdk.services.secretsmanager.model.SecretsManagerException: User: arn:aws:sts::754256621582:assumed-role/cloud-platform-irsa-6852dfe05c1167f2-live/aws-sdk-java-1773998691519 is not authorized to perform: secretsmanager:PutSecretValue on resource: arn:aws:secretsmanager:eu-west-2:953751538119:secret:ingestion-api-auth-token-olmeRm because no resource-based policy allows the secretsmanager:PutSecretValue action (Service: SecretsManager, Status Code: 400, Request ID: 238a8219-a4b7-4f1c-8da5-80c93b63c9ec) (SDK Attempt Count: 1)
