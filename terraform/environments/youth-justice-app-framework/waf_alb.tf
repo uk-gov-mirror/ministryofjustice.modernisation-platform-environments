8d19d56d8b241b5a0fb42633f074052c7cfa6611
ttipler <thomas.tipler@necsws.com>
2026-03-23 10:47:49 +0000

    turn on pilot in preprod

--- terraform/environments/youth-justice-app-framework/waf_alb.tf
+++ terraform/environments/youth-justice-app-framework/waf_alb.tf
@@ -48,11 +48,7 @@ module "waf" {
       name         = "whitelist-ip"
       priority     = 2
       description  = "Whitelisted IP addresses"
-      ip_addresses = concat(["66.103.29.115/32"], [
-        data.aws_subnet.private_subnets_a.cidr_block,
-        data.aws_subnet.private_subnets_b.cidr_block,
-        data.aws_subnet.private_subnets_c.cidr_block
-      ])
+       ip_addresses = ["66.103.29.115/32"]
     }
   }
 
