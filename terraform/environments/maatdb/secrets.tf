9865f880988ebc16f42b42d8f9094411569fb766
Ramya Manuneethi <ramya.manuneethi@opencastsoftware.com>
2026-03-23 12:04:02 +0000

    changed is-production correct local variable

--- terraform/environments/maatdb/secrets.tf
+++ terraform/environments/maatdb/secrets.tf
@@ -1,6 +1,6 @@
 #### This file can be used to store secrets specific to the member account ####
   resource "aws_secretsmanager_secret" "maatdb_maintenance_slack_secrets" {
-    count = local.is_production ? 0 : 1
+    count = local.is-production ? 0 : 1
     name        = "${local.application_name}-${local.environment}-rds-maintenance-slack"
     description = "Slack webhooks for RDS maintenance notifications"
 
@@ -10,7 +10,7 @@
   }
 
   resource "aws_secretsmanager_secret_version" "maatdb_maintenance_slack_secrets_value" {
-    count = local.is_production ? 0 : 1
+    count = local.is-production ? 0 : 1
     secret_id = aws_secretsmanager_secret.maatdb_maintenance_slack_secrets[0].id
 
     secret_string = jsonencode({
