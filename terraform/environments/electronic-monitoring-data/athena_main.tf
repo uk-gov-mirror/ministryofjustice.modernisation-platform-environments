a55e1d87e7dc5491a5287d57981a0a6b493e2106
jono-basford-moj <jono.basford@justice.gov.uk>
2026-03-25 17:11:16 +0000

    Added athena table and fixed bucket attribute call

--- terraform/environments/electronic-monitoring-data/athena_main.tf
+++ terraform/environments/electronic-monitoring-data/athena_main.tf
@@ -92,3 +92,52 @@ resource "aws_athena_workgroup" "cadt-historic-dev" {
   }
 }
 
+
+resource "aws_glue_catalog_database" "ears_sars_audit_db" {
+  count = local.is-development || local.is-preproduction ? 1 : 0  
+  name = "ears_sars_audit"
+}
+resource "aws_glue_catalog_table" "ears_sars_audit_table" {
+  count = local.is-development || local.is-preproduction ? 1 : 0
+  name          = "reports_requested"
+  database_name = aws_glue_catalog_database.ears_sars_audit_db.name
+  table_type    = "EXTERNAL_TABLE"
+
+  parameters = {
+    "classification"              = "json"
+    "projection.enabled"          = "true"
+    "projection.year.type"        = "integer"
+    "projection.year.range"       = "2025,2040"
+    "projection.month.type"       = "integer"
+    "projection.month.range"      = "1,12"
+    "projection.day.type"         = "integer"
+    "projection.day.range"        = "1,31"
+    
+    "storage.location.template" = "s3://${module.s3-logging-bucket.bucket.id}/ears_sars/$${year}/$${month}/$${day}/"
+  }
+  storage_descriptor {
+    location      = "s3://${module.s3-logging-bucket.bucket.id}/ears_sars/"
+    input_format  = "org.apache.hadoop.mapred.TextInputFormat"
+    output_format = "org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat"
+
+    ser_de_info {
+      serialization_library = "org.openx.data.jsonserde.JsonSerDe"
+    }
+    columns {
+      name = "body"
+      type = "string"
+    }
+  }
+  partition_keys {
+    name = "year"
+    type = "string"
+  }
+  partition_keys {
+    name = "month"
+    type = "string"
+  }
+  partition_keys {
+    name = "day"
+    type = "string"
+  }
+}
