424f98816e50f1b7b921ae60a3275a35d77d5223
matt heery <matt.heery@justice.gov.uk>
2026-03-25 09:49:37 +0000

    feat: add cloud platform role

--- terraform/environments/electronic-monitoring-data/share_cloud_platform.tf
+++ terraform/environments/electronic-monitoring-data/share_cloud_platform.tf
@@ -78,6 +78,7 @@ locals {
     ] : local.is-preproduction ? [
     "arn:aws:iam::${local.account_ids["cloud-platform"]}:role/${var.cloud-platform-crime-matching-api-iam-preprod}",
   ] : []
+  iam_role_validation_db = local.is-test ? "arn:aws:iam::${local.account_ids["cloud-platform"]}:role/cloud-platform-irsa-7255c33b35507f31-live" : ""
 }
 
 variable "cloud-platform-iam-dev" {
@@ -134,6 +135,59 @@ resource "aws_lakeformation_resource" "data_bucket" {
   role_arn = module.lakeformation_registration_iam_role.arn
 }
 
+module "emd_validation_db_role" {
+  #checkov:skip=CKV_TF_1:Module registry does not support commit hashes for versions
+  #checkov:skip=CKV_TF_2:Module registry does not support tags for versions
+  count   = local.is-test ? 1 : 0
+  source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role"
+  version = "5.48.0"
+
+  trusted_role_arns = flatten([
+    data.aws_iam_roles.mod_plat_roles.arns,
+    local.iam_role_validation_db,
+  ])
+
+  create_role       = true
+  role_requires_mfa = false
+
+  role_name = "emd_validation_db_read_data_${local.environment_shorthand}"
+
+  tags = local.tags
+}
+
+resource "aws_lakeformation_permissions" "em_data_validation_db" {
+  count       = local.is-test ? 1 : 0
+  principal   = module.emd_validation_db_role[0].iam_role_arn
+  permissions = ["DESCRIBE"]
+  database {
+    name = "validation${local.dbt_suffix}"
+  }
+}
+
+resource "aws_lakeformation_permissions" "em_data_validation_table" {
+  count       = local.is-test ? 1 : 0
+  principal   = module.emd_validation_db_role[0].iam_role_arn
+  permissions = ["DESCRIBE"]
+  table {
+    database_name = "validation${local.dbt_suffix}"
+    wildcard      = true
+  }
+}
+
+resource "aws_lakeformation_persmissions" "em_data_validation_s3" {
+  count = local.is-test ? 1 : 0
+  principal = module.emd_validation_db_role[0].iam_role_arn
+  permissions = ["DATA_LOCATION_ACCESS"]
+  data_location {
+    arn = module.s3-create-a-derived-table-bucket.bucket.arn
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "standard_athena_access_em_data_validation" {
+  count      = local.is-test ? 1 : 0
+  policy_arn = aws_iam_policy.standard_athena_access.arn
+  role       = module.emd_validation_db_role[0].iam_role_name
+}
 
 module "emdi_trail_maps_role" {
   #checkov:skip=CKV_TF_1:Module registry does not support commit hashes for versions
