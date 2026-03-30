2f96af13142d756fdb7515e38f3181444e549d9d
jono-basford-moj <jono.basford@justice.gov.uk>
2026-03-30 11:47:42 +0100

    updated memory

--- terraform/environments/electronic-monitoring-data/ecs_fargate.tf
+++ terraform/environments/electronic-monitoring-data/ecs_fargate.tf
@@ -131,8 +131,8 @@ resource "aws_ecs_task_definition" "emds-gdpr-structured-data-deletion" {
   family                   = "emds_gdpr_structured_data_deletion_family"
   requires_compatibilities = ["FARGATE"]
   network_mode             = "awsvpc"
-  cpu                      = "1024"
-  memory                   = "2048"
+  cpu                      = 2048
+  memory                   = 4096
   execution_role_arn       = aws_iam_role.ecs_gdpr_execution_role.arn
   task_role_arn            = aws_iam_role.ecs_gdpr_execution_role.arn
 
