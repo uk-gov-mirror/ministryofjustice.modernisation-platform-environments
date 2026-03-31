133dd682e8880d1a3419cb2dae00d370a40cb23b
Luke Harper <luke.harper4@justice.gov.uk>
2026-03-31 09:41:15 +0100

    CDIA-40 Prepare MP for preprod and prod.

--- terraform/environments/hmpps-court-data-ingestion-api/lambda.tf
+++ terraform/environments/hmpps-court-data-ingestion-api/lambda.tf
@@ -20,7 +20,7 @@ module "authorizer_lambda" {
   publish = true
 
   environment_variables = {
-    SECRET_ID = module.secret_ingestion_api_auth_token.secret_arn
+    SECRET_ID = local.environment_configuration[local.environment].secret_ingestion_api_auth_token_secret_arn
     SQS_URL   = "https://sqs.eu-west-2.amazonaws.com/${data.aws_secretsmanager_secret_version.cloud_platform_account_id.secret_string}/${local.environment_configuration[local.environment].cloud_platform_sqs_queue_name}"
   }
 
@@ -29,7 +29,7 @@ module "authorizer_lambda" {
     secrets = {
       effect    = "Allow"
       actions   = ["secretsmanager:GetSecretValue"]
-      resources = [module.secret_ingestion_api_auth_token.secret_arn]
+      resources = [module.secret_ingestion_api_auth_token_dev.secret_arn, module.secret_ingestion_api_auth_token_preprod.secret_arn, module.secret_ingestion_api_auth_token_prod.secret_arn]
     }
     kms = {
       effect    = "Allow"
