f2bf4f0fe4a58f82b09daac6c1a17731288ad3ce
Ramya Manuneethi <ramya.manuneethi@opencastsoftware.com>
2026-03-31 15:24:15 +0100

    exclude guard duty scan

--- terraform/environments/ccms-ebs/guardduty-s3.tf
+++ terraform/environments/ccms-ebs/guardduty-s3.tf
@@ -2,139 +2,139 @@
 # GuardDuty Malware Protection Plans
 # ---------------------------------------------
 
-resource "aws_guardduty_malware_protection_plan" "s3_scan_bucket2" {
-  role = data.aws_iam_role.guardduty_s3_scan.arn
+#resource "aws_guardduty_malware_protection_plan" "s3_scan_bucket2" {
+#  role = data.aws_iam_role.guardduty_s3_scan.arn
 
-  protected_resource {
-    s3_bucket {
-      bucket_name = module.s3-bucket-logging.bucket.id
-    }
-  }
+#  protected_resource {
+#    s3_bucket {
+#      bucket_name = module.s3-bucket-logging.bucket.id
+#    }
+#  }
 
-  actions {
-    tagging {
-      status = "ENABLED"
-    }
-  }
+#  actions {
+#    tagging {
+#      status = "ENABLED"
+#    }
+#  }
 
-  tags = merge(local.tags,
-    { Name = lower(format("s3-%s-%s-awsgaurdduty-mpp", "${local.application_name}", local.environment)) }
-  )
+#  tags = merge(local.tags,
+#    { Name = lower(format("s3-%s-%s-awsgaurdduty-mpp", "${local.application_name}", local.environment)) }
+#  )
 
-  depends_on = [module.s3-bucket-logging]
-}
+#  depends_on = [module.s3-bucket-logging]
+#}
 
-resource "aws_guardduty_malware_protection_plan" "s3_scan_bucket3" {
-  role = data.aws_iam_role.guardduty_s3_scan.arn
+#resource "aws_guardduty_malware_protection_plan" "s3_scan_bucket3" {
+#  role = data.aws_iam_role.guardduty_s3_scan.arn
 
-  protected_resource {
-    s3_bucket {
-      bucket_name = module.s3-bucket-dbbackup.bucket.id
-    }
-  }
+#  protected_resource {
+#    s3_bucket {
+#      bucket_name = module.s3-bucket-dbbackup.bucket.id
+#    }
+#  }
 
-  actions {
-    tagging {
-      status = "ENABLED"
-    }
-  }
+#  actions {
+#    tagging {
+#      status = "ENABLED"
+#    }
+#  }
 
-  tags = merge(local.tags,
-    { Name = lower(format("s3-%s-%s-awsgaurdduty-mpp", "${local.application_name}", local.environment)) }
-  )
+#  tags = merge(local.tags,
+#    { Name = lower(format("s3-%s-%s-awsgaurdduty-mpp", "${local.application_name}", local.environment)) }
+#  )
 
-  depends_on = [module.s3-bucket-dbbackup]
-}
+#  depends_on = [module.s3-bucket-dbbackup]
+#}
 
-resource "aws_guardduty_malware_protection_plan" "s3_scan_bucket4" {
-  role = data.aws_iam_role.guardduty_s3_scan.arn
+#resource "aws_guardduty_malware_protection_plan" "s3_scan_bucket4" {
+#  role = data.aws_iam_role.guardduty_s3_scan.arn
 
-  protected_resource {
-    s3_bucket {
-      bucket_name = aws_s3_bucket.lambda_payment_load.id
-    }
-  }
+#  protected_resource {
+#    s3_bucket {
+#      bucket_name = aws_s3_bucket.lambda_payment_load.id
+#    }
+#  }
 
-  actions {
-    tagging {
-      status = "ENABLED"
-    }
-  }
+#  actions {
+#    tagging {
+#      status = "ENABLED"
+#    }
+#  }
 
-  tags = merge(local.tags,
-    { Name = lower(format("s3-%s-%s-awsgaurdduty-mpp", "${local.application_name}", local.environment)) }
-  )
+#  tags = merge(local.tags,
+#    { Name = lower(format("s3-%s-%s-awsgaurdduty-mpp", "${local.application_name}", local.environment)) }
+#  )
 
-  depends_on = [aws_s3_bucket.lambda_payment_load]
-}
+#  depends_on = [aws_s3_bucket.lambda_payment_load]
+#}
 
-resource "aws_guardduty_malware_protection_plan" "s3_scan_bucket5" {
-  for_each = aws_s3_bucket.buckets
-  role     = data.aws_iam_role.guardduty_s3_scan.arn
+#resource "aws_guardduty_malware_protection_plan" "s3_scan_bucket5" {
+#  for_each = aws_s3_bucket.buckets
+#  role     = data.aws_iam_role.guardduty_s3_scan.arn
 
-  protected_resource {
-    s3_bucket {
-      bucket_name = each.value.id
-    }
-  }
+#  protected_resource {
+#    s3_bucket {
+#      bucket_name = each.value.id
+#    }
+#  }
 
-  actions {
-    tagging {
-      status = "ENABLED"
-    }
-  }
+#  actions {
+#    tagging {
+#      status = "ENABLED"
+#    }
+#  }
 
-  tags = merge(local.tags,
-    { Name = lower(format("s3-%s-%s-awsgaurdduty-mpp", "${local.application_name}", local.environment)) }
-  )
+#  tags = merge(local.tags,
+#    { Name = lower(format("s3-%s-%s-awsgaurdduty-mpp", "${local.application_name}", local.environment)) }
+#  )
 
-  depends_on = [aws_s3_bucket.buckets]
-}
+#  depends_on = [aws_s3_bucket.buckets]
+#}
 
-resource "aws_guardduty_malware_protection_plan" "s3_scan_bucket6" {
-  role = data.aws_iam_role.guardduty_s3_scan.arn
+#resource "aws_guardduty_malware_protection_plan" "s3_scan_bucket6" {
+#  role = data.aws_iam_role.guardduty_s3_scan.arn
 
-  protected_resource {
-    s3_bucket {
-      bucket_name = aws_s3_bucket.red_button_data.id
-    }
-  }
+#  protected_resource {
+#    s3_bucket {
+#      bucket_name = aws_s3_bucket.red_button_data.id
+#    }
+#  }
 
-  actions {
-    tagging {
-      status = "ENABLED"
-    }
-  }
+#  actions {
+#    tagging {
+#      status = "ENABLED"
+#    }
+#  }
 
-  tags = merge(local.tags,
-    { Name = lower(format("s3-%s-%s-awsgaurdduty-mpp", "${local.application_name}", local.environment)) }
-  )
+#  tags = merge(local.tags,
+#    { Name = lower(format("s3-%s-%s-awsgaurdduty-mpp", "${local.application_name}", local.environment)) }
+#  )
 
-  depends_on = [aws_s3_bucket.red_button_data]
-}
+#  depends_on = [aws_s3_bucket.red_button_data]
+#}
 
-resource "aws_guardduty_malware_protection_plan" "s3_scan_bucket7" {
-  role = data.aws_iam_role.guardduty_s3_scan.arn
+#resource "aws_guardduty_malware_protection_plan" "s3_scan_bucket7" {
+#  role = data.aws_iam_role.guardduty_s3_scan.arn
 
-  protected_resource {
-    s3_bucket {
-      bucket_name = aws_s3_bucket.ccms_ebs_shared.id
-    }
-  }
+#  protected_resource {
+#    s3_bucket {
+#      bucket_name = aws_s3_bucket.ccms_ebs_shared.id
+#    }
+#  }
 
-  actions {
-    tagging {
-      status = "ENABLED"
-    }
-  }
+#  actions {
+#    tagging {
+#      status = "ENABLED"
+#    }
+#  }
 
-  tags = merge(local.tags,
-    { Name = lower(format("s3-%s-%s-awsgaurdduty-mpp", "${local.application_name}", local.environment)) }
-  )
+#  tags = merge(local.tags,
+#    { Name = lower(format("s3-%s-%s-awsgaurdduty-mpp", "${local.application_name}", local.environment)) }
+#  )
 
-  depends_on = [aws_s3_bucket.ccms_ebs_shared]
-}
+#  depends_on = [aws_s3_bucket.ccms_ebs_shared]
+#}
 
-data "aws_iam_role" "guardduty_s3_scan" {
-  name = "GuardDutyS3MalwareProtectionRole"
-}
+#data "aws_iam_role" "guardduty_s3_scan" {
+#  name = "GuardDutyS3MalwareProtectionRole"
+#}
