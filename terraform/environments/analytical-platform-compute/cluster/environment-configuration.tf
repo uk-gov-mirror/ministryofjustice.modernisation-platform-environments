30f024b301d4c2f8a8443dcab36eb65a184223c5
Raghavendra Kamath <raghavendrakamath.kantavar@justice.gov.uk>
2026-04-01 15:37:02 +0100

    upgrading test cluster/addons to 1.35

--- terraform/environments/analytical-platform-compute/cluster/environment-configuration.tf
+++ terraform/environments/analytical-platform-compute/cluster/environment-configuration.tf
@@ -53,18 +53,18 @@ locals {
 
       /* EKS */
       eks_sso_access_role = "modernisation-platform-developer"
-      eks_cluster_version = "1.34"
+      eks_cluster_version = "1.35"
       eks_node_version    = "1.51.0-47438798"
       eks_cluster_addon_versions = {
-        kube_proxy                        = "v1.34.1-eksbuild.2"
-        aws_efs_csi_driver                = "v2.1.15-eksbuild.1"
-        aws_network_flow_monitoring_agent = "v1.1.1-eksbuild.1"
-        eks_node_monitoring_agent         = "v1.4.2-eksbuild.1"
-        coredns                           = "v1.12.4-eksbuild.1"
-        eks_pod_identity_agent            = "v1.3.10-eksbuild.1"
+        kube_proxy                        = "v1.35.2-eksbuild.4"
+        aws_efs_csi_driver                = "v2.3.1-eksbuild.1"
+        aws_network_flow_monitoring_agent = "v1.1.3-eksbuild.2"
+        eks_node_monitoring_agent         = "v1.6.2-eksbuild.1"
+        coredns                           = "v1.13.2-eksbuild.4"
+        eks_pod_identity_agent            = "v1.3.10-eksbuild.2"
         aws_guardduty_agent               = "v1.12.1-eksbuild.2"
-        aws_ebs_csi_driver                = "v1.53.0-eksbuild.1"
-        vpc_cni                           = "v1.20.5-eksbuild.1"
+        aws_ebs_csi_driver                = "v1.57.1-eksbuild.1"
+        vpc_cni                           = "v1.21.1-eksbuild.5"
       }
 
       helm_chart_version = {
