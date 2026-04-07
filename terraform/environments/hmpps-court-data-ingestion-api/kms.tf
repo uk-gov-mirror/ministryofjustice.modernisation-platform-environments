fe035cd7c34b10e64044bc666b6d09ff80074976
Luke Harper <luke.harper4@justice.gov.uk>
2026-04-07 08:59:01 +0100

    CDIA-36 Allow KMS access.

--- terraform/environments/hmpps-court-data-ingestion-api/kms.tf
+++ terraform/environments/hmpps-court-data-ingestion-api/kms.tf
@@ -4,7 +4,61 @@ module "secrets_kms" {
 
   aliases                 = ["${local.application_name}-secrets"]
   description             = "KMS key for ${local.application_name} secrets"
-  enable_default_policy   = true
   deletion_window_in_days = 7
   tags                    = local.tags
+}
+
+resource "aws_kms_key_policy" "key_policy" {
+  key_id = module.secrets_kms.key_id
+  policy = jsonencode({
+    Id = "example"
+    Statement = [
+      {
+        "Sid": "Default",
+        "Effect": "Allow",
+        "Principal": {
+          "AWS": "arn:aws:iam::953751538119:root"
+        },
+        "Action": "kms:*",
+        "Resource": "*"
+      },
+      {
+        "Sid": "Allow use of the key for SSM only",
+        "Effect": "Allow",
+        "Principal": {
+            "AWS": "arn:aws:iam::754256621582:root"
+        },
+        "Action": [
+            "kms:Encrypt",
+            "kms:Decrypt",
+            "kms:ReEncrypt*",
+            "kms:GenerateDataKey*"
+        ],
+        "Resource": "*"
+    },
+    {
+        "Sid": "Allow reading of key metadata",
+        "Effect": "Allow",
+        "Principal": {
+            "AWS": "arn:aws:iam::754256621582:root"
+        },
+        "Action": "kms:DescribeKey",
+        "Resource": "*"
+    },
+    {
+        "Sid": "Allow attachment of persistent resources",
+        "Effect": "Allow",
+        "Principal": {
+            "AWS": "arn:aws:iam::754256621582:root"
+        },
+        "Action": [
+            "kms:CreateGrant",
+            "kms:ListGrants",
+            "kms:RevokeGrant"
+        ],
+        "Resource": "*"
+    }
+    ]
+    Version = "2012-10-17"
+  })
 }
