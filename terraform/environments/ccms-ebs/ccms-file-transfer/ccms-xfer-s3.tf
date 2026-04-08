2d112fe5966c0b0f96f016514e9389b869b19ad2
Sanjay Baranwal <sanjay.baranwal@justice.gov.uk>
2026-04-08 15:10:31 +0100

    fix: corrected lambda function

--- terraform/environments/ccms-ebs/ccms-file-transfer/ccms-xfer-s3.tf
+++ terraform/environments/ccms-ebs/ccms-file-transfer/ccms-xfer-s3.tf
@@ -120,7 +120,7 @@ resource "aws_s3_bucket_notification" "sftp_client1_file_lambda_notification" {
   # }
 
   lambda_function {
-    lambda_function_arn = module.s3-file-lambda.function.arn
+    lambda_function_arn = aws_lambda_function.process_file_from_bucket.arn
     events              = ["s3:ObjectCreated:*"]
     filter_prefix = "inbound/"
     filter_suffix       = ".log" #asked a question
