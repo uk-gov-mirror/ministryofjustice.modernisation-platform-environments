e8fa6f1d068b057ce0c9114a18be267f8bf6312e
Lev Gorbunov <lev.gorbunov@justice.gov.uk>
2026-04-08 14:35:06 +0100

    sed delimiter

--- scripts/enhance-scp-error.sh
+++ scripts/enhance-scp-error.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sed -e 's/p-w8ht8v1x/p-w8ht8v1x (Enforce mandatory tags), please see our tagging policy https://cloud-optimisation-and-accountability.justice.gov.uk/documentation/finops-and-greenops-at-moj/standards/tagging.html/g'
+sed -E 's|p-w8ht8v1x|p-w8ht8v1x (Enforce mandatory tags), please see our tagging policy https://cloud-optimisation-and-accountability.justice.gov.uk/documentation/finops-and-greenops-at-moj/standards/tagging.html|g'
