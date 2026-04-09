074c5ccdbf584f145975fb374dbc95d260786d26
ghill26 <George.Hill2@justice.gov.uk>
2026-04-09 11:09:26 +0100

    Update S3 Module pinned version

--- terraform/modules/baseline/s3_bucket.tf
+++ terraform/modules/baseline/s3_bucket.tf
@@ -31,7 +31,7 @@ module "s3_bucket" {
 
   for_each = var.s3_buckets
 
-  source = "git::https://github.com/ministryofjustice/modernisation-platform-terraform-s3-bucket?ref=4dca9f4"
+  source = "git::https://github.com/ministryofjustice/modernisation-platform-terraform-s3-bucket?ref=v9.0.1"
 
   providers = {
     aws.bucket-replication = aws
