b74f806cec978834d113de7e3689ed7c71d40dfd
Raghavendra Kamath <raghavvidya@gmail.com>
2026-04-09 08:09:51 +0100

    Revert "testing the role"
    This reverts commit 31b54af2987a706369d0ff0562d9d7e06ce1694e.

--- terraform/environments/analytical-platform-compute/cluster/providers.tf
+++ terraform/environments/analytical-platform-compute/cluster/providers.tf
@@ -3,7 +3,7 @@ provider "aws" {
   alias  = "analytical-platform-compute-eu-west-1"
   region = "eu-west-1"
   assume_role {
-    role_arn = !can(regex("githubactionsplanrolesession|githubactionsrolesession|AdministratorAccess|user", data.aws_caller_identity.original_session.arn)) ? null : can(regex("user", data.aws_caller_identity.original_session.arn)) ? "arn:aws:iam::${local.environment_management.account_ids[terraform.workspace]}:role/${var.collaborator_access}" : "arn:aws:iam::${data.aws_caller_identity.original_session.id}:role/MemberInfrastructureAccess"
+    role_arn = !can(regex("githubactionsrolesession|AdministratorAccess|user", data.aws_caller_identity.original_session.arn)) ? null : can(regex("user", data.aws_caller_identity.original_session.arn)) ? "arn:aws:iam::${local.environment_management.account_ids[terraform.workspace]}:role/${var.collaborator_access}" : "arn:aws:iam::${data.aws_caller_identity.original_session.id}:role/MemberInfrastructureAccess"
   }
 }
 
