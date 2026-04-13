cf08bd74fbeea5c4596eca1df6e4d6ba7fa54e03
Sanjay Baranwal <sanjay.baranwal@justice.gov.uk>
2026-04-13 22:46:50 +0100

    fix: cloudwatch logs and alarm added

--- terraform/environments/ccms-ebs/ccms-file-transfer/member-data.tf
+++ terraform/environments/ccms-ebs/ccms-file-transfer/member-data.tf
@@ -2,6 +2,10 @@ data "aws_sns_topic" "s3_topic" {
   name = "s3-event-notification-topic"
 }
 
+data "aws_sns_topic" "s3_topic" {
+  name = "ccms-ebs-ec2-alerts"
+}
+
 data "aws_s3_bucket" "logging_bucket" {
   bucket = "${local.application_name}-${local.environment}-logging"
 }
