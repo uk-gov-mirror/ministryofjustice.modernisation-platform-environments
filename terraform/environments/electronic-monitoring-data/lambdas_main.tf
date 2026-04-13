641dd8ed7622d3157b589266e2f69bffa9708ada
Khristiania <khristiania.raihan@justice.gov.uk>
2026-04-13 16:39:02 +0100

    moved names to a local file

--- terraform/environments/electronic-monitoring-data/lambdas_main.tf
+++ terraform/environments/electronic-monitoring-data/lambdas_main.tf
@@ -540,18 +540,18 @@ module "mdss_daily_failure_digest" {
     CLEAN_DLT_DLQ_NAME  = aws_sqs_queue.clean_dlt_load_dlq.name
     LOAD_FMS_DLQ_NAME   = module.load_fms_event_queue.sqs_dlq.name
 
-    PROCESS_LANDING_BUCKET_FILES_FMS_GENERAL_DLQ_NAME  = "-process_landing_bucket_files_fms_general-dlq"
-    PROCESS_LANDING_BUCKET_FILES_FMS_HO_DLQ_NAME       = "-process_landing_bucket_files_fms_ho-dlq"
-    PROCESS_LANDING_BUCKET_FILES_FMS_SPECIALS_DLQ_NAME = "-process_landing_bucket_files_fms_specials-dlq"
+    PROCESS_LANDING_BUCKET_FILES_FMS_GENERAL_DLQ_NAME  = local.live_feed_dlq_names.process_landing_bucket_files_fms_general
+    PROCESS_LANDING_BUCKET_FILES_FMS_HO_DLQ_NAME       = local.live_feed_dlq_names.process_landing_bucket_files_fms_ho
+    PROCESS_LANDING_BUCKET_FILES_FMS_SPECIALS_DLQ_NAME = local.live_feed_dlq_names.process_landing_bucket_files_fms_specials
 
-    PROCESS_LANDING_BUCKET_FILES_MDSS_GENERAL_DLQ_NAME  = "-process_landing_bucket_files_mdss_general-dlq"
-    PROCESS_LANDING_BUCKET_FILES_MDSS_HO_DLQ_NAME       = "-process_landing_bucket_files_mdss_ho-dlq"
-    PROCESS_LANDING_BUCKET_FILES_MDSS_SPECIALS_DLQ_NAME = "-process_landing_bucket_files_mdss_specials-dlq"
+    PROCESS_LANDING_BUCKET_FILES_MDSS_GENERAL_DLQ_NAME  = local.live_feed_dlq_names.process_landing_bucket_files_mdss_general
+    PROCESS_LANDING_BUCKET_FILES_MDSS_HO_DLQ_NAME       = local.live_feed_dlq_names.process_landing_bucket_files_mdss_ho
+    PROCESS_LANDING_BUCKET_FILES_MDSS_SPECIALS_DLQ_NAME = local.live_feed_dlq_names.process_landing_bucket_files_mdss_specials
 
-    SCAN_DLQ_NAME                  = "-scan-dlq"
-    PROCESS_FMS_METADATA_DLQ_NAME  = "-process_fms_metadata-dlq"
-    FORMAT_FMS_JSON_DLQ_NAME       = aws_sqs_queue.format_fms_json_event_dlq.name
-    PUSH_DATA_EXPORT_TO_P1_DLQ_NAME = "-push_data_export_to_p1-dlq"
+    SCAN_DLQ_NAME                   = local.live_feed_dlq_names.scan
+    PROCESS_FMS_METADATA_DLQ_NAME   = local.live_feed_dlq_names.process_fms_metadata
+    FORMAT_FMS_JSON_DLQ_NAME        = aws_sqs_queue.format_fms_json_event_dlq.name
+    PUSH_DATA_EXPORT_TO_P1_DLQ_NAME = local.live_feed_dlq_names.push_data_export_to_p1
 
     LOAD_FMS_FUNCTION_NAME             = module.load_fms_lambda.lambda_function_name
     PROCESS_FMS_METADATA_FUNCTION_NAME = module.process_fms_metadata.lambda_function_name
