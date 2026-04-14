baa3f2f59848c0ef853e7eee11b3a5b39a313051
Nick-MoJ-Pro <nicholas.aidudo@justice.gov.uk>
2026-04-14 12:13:27 +0100

    feat: added compliance mode

--- terraform/environments/ccms-ebs/ccms-s3.tf
+++ terraform/environments/ccms-ebs/ccms-s3.tf
@@ -196,8 +196,8 @@ resource "aws_s3_bucket_object_lock_configuration" "dbbackup" {
 
   rule {
     default_retention {
-      mode = "GOVERNANCE"
-      days = local.application_data.accounts[local.environment].rman_s3_lifecycle_days_expiration_current
+      mode = "COMPLIANCE"
+      days = local.environment == "production" ? 90 : 30
     }
   }
 }
