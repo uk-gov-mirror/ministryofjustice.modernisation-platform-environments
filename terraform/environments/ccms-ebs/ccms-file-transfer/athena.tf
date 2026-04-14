1abd93630163edfa37db33a0a53840f827c256f0
Sanjay Baranwal <sanjay.baranwal@justice.gov.uk>
2026-04-14 16:07:05 +0100

    fix: athena and ecr repo details added

--- terraform/environments/ccms-ebs/ccms-file-transfer/athena.tf
+++ terraform/environments/ccms-ebs/ccms-file-transfer/athena.tf
@@ -1,20 +1,20 @@
 resource "aws_athena_database" "lb-access-logs" {
   name   = "loadbalancer_access_logs"
-  bucket = module.s3-bucket-logging.bucket.id
+  bucket = data.aws_s3_bucket.logging_bucket.id
   encryption_configuration {
     encryption_option = "SSE_S3"
   }
 }
 
 resource "aws_athena_workgroup" "lb-access-logs" {
-  name = lower(format("%s-%s-lb-access-logs", local.application_name, local.environment))
+  name = lower(format("%s-sftp-barclaycard-%s-lb-access-logs", local.application_name, local.environment))
 
   configuration {
     enforce_workgroup_configuration    = true
     publish_cloudwatch_metrics_enabled = true
 
     result_configuration {
-      output_location = "s3://${module.s3-bucket-logging.bucket.id}/output/"
+      output_location = "s3://${data.aws_s3_bucket.logging_bucket.id}/output/"
       encryption_configuration {
         encryption_option = "SSE_S3"
       }
@@ -23,47 +23,15 @@ resource "aws_athena_workgroup" "lb-access-logs" {
 }
 
 # SQL query to creates the table in the athena db, these queries needs to be executed manually after creation
-resource "aws_athena_named_query" "main_table_ebsapp" {
-  name      = lower(format("%s-%s-create-table", local.application_name, local.environment))
-  workgroup = aws_athena_workgroup.lb-access-logs.id
-  database  = aws_athena_database.lb-access-logs.name
-  query = templatefile(
-    "./templates/create_table.sql",
-    {
-      bucket     = module.s3-bucket-logging.bucket.id
-      key        = local.lb_log_prefix_ebsapp
-      account_id = data.aws_caller_identity.current.id
-      region     = data.aws_region.current.id
-    }
-  )
-}
-
-# SQL query to count the number of HTTP GET requests to the loadbalancer grouped by IP, these queries needs to be executed manually after creation
-resource "aws_athena_named_query" "http_requests_ebsapp" {
-  name      = lower(format("%s-%s-http-get-requests-ebsapp", local.application_name, local.environment))
-  workgroup = aws_athena_workgroup.lb-access-logs.id
-  database  = aws_athena_database.lb-access-logs.name
-  query = templatefile(
-    "./templates/lb_http_gets.sql",
-    {
-      bucket     = module.s3-bucket-logging.bucket.id
-      key        = local.lb_log_prefix_ebsapp
-      account_id = data.aws_caller_identity.current.id
-      region     = data.aws_region.current.id
-    }
-  )
-}
-
-# SQL query to creates the table in the athena db, these queries needs to be executed manually after creation
-resource "aws_athena_named_query" "main_table_ebsapp_internal" {
-  name      = lower(format("%s-%s-create-table-internal", local.application_name, local.environment))
+resource "aws_athena_named_query" "main_table_sftp_barclaycard_internal_query" {
+  name      = lower(format("%s-sftp-barclaycard-%s-create-table", local.application_name, local.environment))
   workgroup = aws_athena_workgroup.lb-access-logs.id
   database  = aws_athena_database.lb-access-logs.name
   query = templatefile(
     "./templates/create_internal_table.sql",
     {
-      bucket     = module.s3-bucket-logging.bucket.id
-      key        = local.lb_log_prefix_ebsapp_internal
+      bucket     = data.aws_s3_bucket.logging_bucket.id
+      key        = local.sftp_barclaycard_bucket_name
       account_id = data.aws_caller_identity.current.id
       region     = data.aws_region.current.id
     }
@@ -71,15 +39,15 @@ resource "aws_athena_named_query" "main_table_ebsapp_internal" {
 }
 
 # SQL query to count the number of HTTP GET requests to the loadbalancer grouped by IP, these queries needs to be executed manually after creation
-resource "aws_athena_named_query" "http_requests_ebsapp_internal" {
-  name      = lower(format("%s-%s-http-get-requests-ebsapp-internal", local.application_name, local.environment))
+resource "aws_athena_named_query" "http_requests_sftp_barclaycard_internal_query" {
+  name      = lower(format("%s-sftp-barclaycard-%s-http-get-requests", local.application_name, local.environment))
   workgroup = aws_athena_workgroup.lb-access-logs.id
   database  = aws_athena_database.lb-access-logs.name
   query = templatefile(
     "./templates/lb_internal_http_gets.sql",
     {
       bucket     = module.s3-bucket-logging.bucket.id
-      key        = local.lb_log_prefix_ebsapp_internal
+      key        = local.sftp_barclaycard_bucket_name
       account_id = data.aws_caller_identity.current.id
       region     = data.aws_region.current.id
     }
