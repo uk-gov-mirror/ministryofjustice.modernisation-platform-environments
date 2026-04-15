c61b0a7cb04c7b8bb700b0606da84e7b30923e8f
jono-basford-moj <jono.basford@justice.gov.uk>
2026-04-15 16:06:23 +0100

    Updated iam role

--- terraform/environments/electronic-monitoring-data/step_functions_iam.tf
+++ terraform/environments/electronic-monitoring-data/step_functions_iam.tf
@@ -115,14 +115,16 @@ data "aws_iam_policy_document" "gdpr_delete_policy_document" {
   statement {
     effect = "Allow"
     actions = [
-      "ecs:DescribeTasks",
+      "events:PutTargets",
+      "events:PutRule",
+      "events:DescribeRule"
     ]
-    resources = ["arn:aws:ecs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:emds_gdpr_structured_data_deletion_job*"]
+    resources = ["arn:aws:events:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:rule/StepFunctionsGetEventsForECSTaskRule"]
   }
 }
 
 resource "aws_iam_policy" "gdpr_delete_iam_policy" {
   count  = local.is-development || local.is-preproduction ? 1 : 0
-  name   = "gdpr_deletetion_step_function_role"
+  name   = "gdpr_deletion_step_function_role"
   policy = data.aws_iam_policy_document.gdpr_delete_policy_document[0].json
 }
