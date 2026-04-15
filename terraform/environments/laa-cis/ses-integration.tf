7bc1bbbfffa11ab89f70f44f4a5e1be0e03cda0c
Liam Rundle <liam.rundle@justice.gov.uk>
2026-04-15 15:13:34 +0100

    records for ses validation

--- terraform/environments/laa-cis/ses-integration.tf
+++ terraform/environments/laa-cis/ses-integration.tf
@@ -3,35 +3,31 @@
 #####################################
 
 locals {
-  ses_domain_identity = "cis-dev.modernisation-platform.service.justice.gov.uk"
+  ses_base_domain      = "modernisation-platform.service.justice.gov.uk"
+  ses_environment_name = try(local.application_data.accounts[local.environment].ses_environment_name, null)
+  ses_domain_identity  = local.ses_environment_name == null ? null : "${local.ses_environment_name}.${local.ses_base_domain}"
+  ses_dkim_tokens      = try(local.application_data.accounts[local.environment].ses_dkim_tokens, [])
+  ses_mail_from_name   = try(local.application_data.accounts[local.environment].ses_mail_from_subdomain, "mail")
 
-  ses_dkim_records = [
-    {
-      name  = "cn3hj46ubipkybq7lags7szz2sb6jdk5._domainkey.cis-dev.modernisation-platform.service.justice.gov.uk"
+  ses_mail_from_domain = local.ses_domain_identity == null ? null : "${local.ses_mail_from_name}.${local.ses_domain_identity}"
+
+  ses_dkim_records = local.ses_domain_identity == null ? [] : [
+    for token in local.ses_dkim_tokens : {
+      name  = "${token}._domainkey.${local.ses_domain_identity}"
       type  = "CNAME"
-      value = "cn3hj46ubipkybq7lags7szz2sb6jdk5.dkim.amazonses.com"
-    },
-    {
-      name  = "m7unpkilg3jsqxhpp5x7hq6qp7rr33pt._domainkey.cis-dev.modernisation-platform.service.justice.gov.uk"
-      type  = "CNAME"
-      value = "m7unpkilg3jsqxhpp5x7hq6qp7rr33pt.dkim.amazonses.com"
-    },
-    {
-      name  = "cpmyvcm6ap2rzfd7lbzwohmwx2ychtx2._domainkey.cis-dev.modernisation-platform.service.justice.gov.uk"
-      type  = "CNAME"
-      value = "cpmyvcm6ap2rzfd7lbzwohmwx2ychtx2.dkim.amazonses.com"
-    },
+      value = "${token}.dkim.amazonses.com"
+    }
   ]
 
   ses_mail_from_mx_record = {
-    name     = "mail.cis-dev.modernisation-platform.service.justice.gov.uk"
+    name     = local.ses_mail_from_domain
     priority = 10
     type     = "MX"
     value    = "feedback-smtp.eu-west-2.amazonses.com"
   }
 
   ses_mail_from_spf_record = {
-    name  = "mail.cis-dev.modernisation-platform.service.justice.gov.uk"
+    name  = local.ses_mail_from_domain
     type  = "TXT"
     value = "v=spf1 include:amazonses.com ~all"
   }
@@ -50,6 +46,7 @@ resource "aws_route53_record" "ses_dkim" {
 
 resource "aws_route53_record" "ses_mail_from_mx" {
   provider = aws.core-network-services
+  count    = local.ses_mail_from_domain == null ? 0 : 1
 
   zone_id = data.aws_route53_zone.network-services.zone_id
   name    = local.ses_mail_from_mx_record.name
@@ -60,6 +57,7 @@ resource "aws_route53_record" "ses_mail_from_mx" {
 
 resource "aws_route53_record" "ses_mail_from_spf" {
   provider = aws.core-network-services
+  count    = local.ses_mail_from_domain == null ? 0 : 1
 
   zone_id = data.aws_route53_zone.network-services.zone_id
   name    = local.ses_mail_from_spf_record.name
