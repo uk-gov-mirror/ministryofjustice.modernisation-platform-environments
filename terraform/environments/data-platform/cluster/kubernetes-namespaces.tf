7e54a65b17feb09b5408be79f9e8d5d3402105dc
tamsinforbes <32667518+tamsinforbes@users.noreply.github.com>
2026-04-17 12:43:51 +0000

    Uncomment original keda_namespace and remove duplicate

--- terraform/environments/data-platform/cluster/kubernetes-namespaces.tf
+++ terraform/environments/data-platform/cluster/kubernetes-namespaces.tf
@@ -47,13 +47,6 @@ module "karpenter_namespace" {
   workload = "system"
 }
 
-module "keda_namespace" {
-  source = "./modules/kubernetes/namespace"
-
-  name     = "keda"
-  workload = "system"
-}
-
 module "external_dns_namespace" {
   source = "./modules/kubernetes/namespace"
 
@@ -75,10 +68,9 @@ module "shared_services_namespace" {
   workload = "system"
 }
 
-# Commenting out KEDA namespace for now as we are not using it yet. We can add it back in when we need it.
-# module "keda_namespace" {
-#   source = "./modules/kubernetes/namespace"
+module "keda_namespace" {
+  source = "./modules/kubernetes/namespace"
 
-#   name     = "keda"
-#   workload = "system"
-# }
+  name     = "keda"
+  workload = "system"
+}
