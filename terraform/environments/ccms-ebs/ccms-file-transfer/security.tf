8e2e16a40f204a2fcfd869f3e4419a6d303d6f77
Sanjay Baranwal <sanjay.baranwal@justice.gov.uk>
2026-04-20 17:58:41 +0100

    fix: adding sg for lambda and connectivity to lb

--- terraform/environments/ccms-ebs/ccms-file-transfer/security.tf
+++ terraform/environments/ccms-ebs/ccms-file-transfer/security.tf
@@ -20,6 +20,16 @@ resource "aws_vpc_security_group_ingress_rule" "sftp_barclaycard_lb_ingress_443"
   to_port     = 443
 }
 
+resource "aws_vpc_security_group_ingress_rule" "sftp_barclaycard_lb_ingress_443" {
+  security_group_id = aws_security_group.sftp_barclaycard_load_balancer.id
+
+  description = "HTTPS from Anywhere - WAF in front of ALB"
+  ip_protocol = "tcp"
+  from_port   = 443
+  to_port     = 443
+  referenced_security_group_id = aws_security_group.process_file_from_bucket_lambda_sg.id
+}
+
 resource "aws_vpc_security_group_egress_rule" "sftp_barclaycard_lb_egress_api" {
   security_group_id = aws_security_group.sftp_barclaycard_load_balancer.id
 
@@ -56,4 +66,24 @@ resource "aws_vpc_security_group_egress_rule" "cluster_fargate_sg_egress_all" {
   ip_protocol = "tcp"
   from_port   = 443
   to_port     = 443
+}
+
+# Fargate Security Group
+resource "aws_security_group" "process_file_from_bucket_lambda_sg" {
+  name        = "${local.application_name}-process-file-from-bucket-lambda-security-group"
+  description = "Controls access from the process file from bucket lambda function"
+  vpc_id      = data.aws_vpc.shared.id
+
+  tags = merge(local.tags,
+    { Name = lower(format("%s-sftp-%s-process-file-from-bucket-lambda-sg", local.application_name, local.environment)) }
+  )
+}
+
+resource "aws_vpc_security_group_egress_rule" "process_file_from_bucket_lambda_sg_egress_all" {
+  security_group_id = aws_security_group.process_file_from_bucket_lambda_sg.id
+
+  ip_protocol = "tcp"
+  from_port   = 443
+  to_port     = 443
+  referenced_security_group_id = aws_security_group.sftp_barclaycard_load_balancer.id
 }
