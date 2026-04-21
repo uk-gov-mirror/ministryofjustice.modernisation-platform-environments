67c2b0943974141a4c8e241b30c13eaf2396f43c
Muhammad-MOJ <MuhammadImran.Khan@justice.gov.uk>
2026-04-21 15:06:01 +0100

    fix

--- terraform/environments/oas/endpoints.tf
+++ terraform/environments/oas/endpoints.tf
@@ -26,7 +26,7 @@ resource "aws_security_group" "vpce_sg" {
     from_port       = 443
     to_port         = 443
     protocol        = "tcp"
-    security_groups = [aws_security_group.ec2_sg.id]
+    security_groups = [aws_security_group.ec2_sg[0].id]
   }
 
 }
