d112c55e7df024fbfd75274abaac8cccccba0cf7
Sanjay Baranwal <sanjay.baranwal@justice.gov.uk>
2026-04-23 14:59:38 +0100

    fix: invoke function allowed from eventbridge

--- terraform/environments/ccms-ebs/ccms-file-transfer/route53.tf
+++ terraform/environments/ccms-ebs/ccms-file-transfer/route53.tf
@@ -8,7 +8,7 @@ resource "aws_route53_record" "route53_record_sftp_bc_nonprod" {
   count    = local.is-production ? 0 : 1
   provider = aws.core-vpc
   zone_id  = data.aws_route53_zone.external.zone_id
-  name     = local.application_data.accounts[local.environment].app_name
+  name     = "${local.application_name}-fin-transfer"
   type     = "A"
 
   alias {
@@ -28,7 +28,7 @@ resource "aws_route53_record" "route53_record_sftp_bc_prod" {
   count    = local.is-production ? 1 : 0
   provider = aws.core-network-services
   zone_id  = data.aws_route53_zone.laa.zone_id
-  name     = local.application_data.accounts[local.environment].app_name
+  name     = "${local.application_name}-fin-transfer"
   type     = "A"
   alias {
     name                   = aws_lb.sftp_bc_load_balancer.dns_name
